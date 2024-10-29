19273ff068263c60605948dbe0e1cef2ae905fd3
Eros Netto Antunes <erosnetto1002@gmail.com>
2024-10-29 10:37:26 -0300

    Testando CV

--- portifolio/src/components/Intro/Intro.jsx
+++ portifolio/src/components/Intro/Intro.jsx
@@ -9,7 +9,7 @@ const Intro = () => {
   // Função para baixar o CV
   const handleDownloadCV = () => {
     const link = document.createElement("a");
-    link.href = "/Currículo Eros Netto Antunes.pdf";
+    link.href = "/Curriculo Eros Netto Antunes.pdf";
     link.download = "Currículo Eros Netto Antunes.pdf";
     document.body.appendChild(link);
     link.click();
